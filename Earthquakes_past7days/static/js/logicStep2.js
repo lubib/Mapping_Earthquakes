e97bc306ecabea8579733dc9bbd6cf480575fa8e
Lubi Voroga <lubi@macbook-air.lan>
2020-09-20 12:44:49 -0400

    adding some styling to the earthquake data

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -7,7 +7,8 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
     maxZoom: 18,
     accessToken: API_KEY
     });
-   
+      // Then we add our 'graymap' tile layer to the map.
+      // streets.addTo(map);
 
     // We create the atelliteStreets view tile layer that will be an option for our map.
 let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -34,9 +35,57 @@ L.control.layers(baseMaps).addTo(map);
 
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+
+// This function returns the style data for each of the earthquakes we plot on
+// the map. We pass the magnitude of the earthquake into a function
+// to calculate the radius.
+function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: "#ffae42",
+      color: "#000000",
+      radius: getRadius(),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+  
+
+// This function determines the radius of the earthquake marker based on its magnitude.
+// Earthquakes with a magnitude of 0 were being plotted with the wrong radius.
+function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 4;
+  }
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data, {
+
+// We turn each feature into a circleMarker on the map.
+
+pointToLayer: function(feature, latlng) {
+    console.log(data);
+    return L.circleMarker(latlng);
+},
+// We set the style for each circleMarker using our styleInfo function.
+style: styleInfo
+}).addTo(map);
 });
 
+// Grabbing our GeoJSON data.
+// d3.json(torontoHoods).then(function(data) {
+//   console.log(data);
+// // Creating a GeoJSON layer with the retrieved data.
+// L.geoJson(data, {
+//   color: "blue",
+//   weight: 1,
+//   fillColor: "#ffffa1",
+//   onEachFeature: function(feature,layer) {
+//     layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3> ");
+//   }
+// }).addTo(map);
+// });
 
 
